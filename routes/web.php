4a2a390d5934853d1ee2994f9cbbf819f41177f1
muhammad.yusuf <muhammad.yusuf@immobisp.com>
2024-04-24 17:21:13 +0700

    del register

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,10 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'dashboard']);
 Route::get('/home/{tb}/{select_angkatan}/{select_periode}', [App\Http\Controllers\HomeController::class, 'dashboard']);
 Route::get('/tabgraf/{tb}/{select_angkatan}/{select_periode}', [App\Http\Controllers\HomeController::class, 'tabgraf']);
 
-Route::get('/register', [App\Http\Controllers\RegisterController::class, 'register'])->name('register');
-Route::post('/register/store', [App\Http\Controllers\RegisterController::class, 'store_from_public'])->name('store user from public');
+// Route::get('/register', [App\Http\Controllers\RegisterController::class, 'register'])->name('register');
+// Route::post('/register/store', [App\Http\Controllers\RegisterController::class, 'store_from_public'])->name('store user from public');
 
-Auth::routes(['register' => false]);
+// Auth::routes(['register' => false]);
 
 Route::get('/profil', [App\Http\Controllers\UserController::class, 'my_profile'])->name('my profile');
 Route::get('/profil/edit', [App\Http\Controllers\UserController::class, 'edit_my_profile'])->name('edit my profile');
