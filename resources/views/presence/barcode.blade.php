b76d66cdd7bc4d9fb491c0ca390f6e4badbf0669
muhammad.yusuf <muhammad.yusuf@immobisp.com>
2024-05-21 10:34:44 +0700

    bug fix

--- resources/views/presence/barcode.blade.php
+++ resources/views/presence/barcode.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Quagga Test</title>
+    <title>Barcode - Presence</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="description" content="" />
@@ -10,11 +10,11 @@
     <link id="pagestyle" href="{{ asset('css/argon-dashboard.css') }}" rel="stylesheet" />
     <script src="https://code.jquery.com/jquery-1.9.0.min.js" integrity="sha256-f6DVw/U4x2+HjgEqw5BZf67Kq/5vudRZuRkljnbF344=" crossorigin="anonymous"></script>
     <script src="https://webrtc.github.io/adapter/adapter-latest.js" type="text/javascript"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.min.js"></script>
 </head>
 <style>
-    #drawingBuffer {
+    .drawingBuffer {
         position: absolute;
         top: 0;
         left: 0;
@@ -29,7 +29,7 @@
             <div id="interactive" class="viewport">
                 <center>
                     <video autoplay="true" preload="auto">
-                        <!-- <canvas width="200" height="100" id="drawingBuffer"></canvas> -->
+                        <canvas width="200" height="100" class="drawingBuffer"></canvas>
                     </video>
                 </center>
                 <div id="log-result"></div>
@@ -37,52 +37,15 @@
         </div>
     </div>
     <script type="text/javascript">
-        Quagga.init({
-            inputStream: {
-                name: "Live",
-                type: "LiveStream",
-                target: document.querySelector('#interactive'),
-                constraints: {
-                    width: 520,
-                    height: 200,
-                    facingMode: "environment" //"environment" for back camera, "user" front camera
-                },
-                area: { // defines rectangle of the detection/localization area
-                    top: "0%", // top offset
-                    right: "0%", // right offset
-                    left: "0%", // left offset
-                    bottom: "0%" // bottom offset
-                },
-                singleChannel: true
-            },
-            decoder: {
-                readers: ["code_128_reader"]
-            }
-        }, function(err) {
-            if (err) {
-                console.log(err)
-                $("#log-result").html("error: " + JSON.stringify(err));
-                return
-            }
-            Quagga.start();
-            Quagga.onDetected(function(result) {
-                $("#log-result").html("detected: " + JSON.stringify(result.codeResult.code));
-                Quagga.stop();
-            });
-            Quagga.onProcessed(function(result) {
-                $("#log-result").html("process: " + JSON.stringify(result));
-            });
-        });
-
         // Quagga.init({
         //     inputStream: {
         //         name: "Live",
         //         type: "LiveStream",
         //         target: document.querySelector('#interactive'),
         //         constraints: {
-        //             width: window.innerWidth,
-        //             height: window.innerHeight,
-        //             facingMode: "environment",
+        //             width: 520,
+        //             height: 200,
+        //             facingMode: "environment" //"environment" for back camera, "user" front camera
         //         },
         //         area: { // defines rectangle of the detection/localization area
         //             top: "0%", // top offset
@@ -90,96 +53,134 @@
         //             left: "0%", // left offset
         //             bottom: "0%" // bottom offset
         //         },
-        //     },
-        //     numOfWorkers: navigator.hardwareConcurrency,
-        //     locate: true,
-        //     frequency: 1,
-        //     debug: {
-        //         drawBoundingBox: true,
-        //         showFrequency: true,
-        //         drawScanline: true,
-        //         showPattern: true
-        //     },
-        //     multiple: false,
-        //     locator: {
-        //         halfSample: false,
-        //         patchSize: "large", // x-small, small, medium, large, x-large
-        //         debug: {
-        //             showCanvas: true,
-        //             showPatches: true,
-        //             showFoundPatches: true,
-        //             showSkeleton: true,
-        //             showLabels: true,
-        //             showPatchLabels: true,
-        //             showRemainingPatchLabels: true,
-        //             boxFromPatches: {
-        //                 showTransformed: true,
-        //                 showTransformedBox: true,
-        //                 showBB: true
-        //             }
-        //         }
+        //         singleChannel: true
         //     },
         //     decoder: {
         //         readers: ["code_128_reader"]
-
         //     }
         // }, function(err) {
         //     if (err) {
-        //         alert("error name:-" + err.name + " & error message:-" + err.message);
+        //         console.log(err)
+        //         $("#log-result").html("error: " + JSON.stringify(err));
+        //         return
         //     }
-
         //     Quagga.start();
+        //     Quagga.onDetected(function(result) {
+        //         $("#log-result").html("detected: " + JSON.stringify(result.codeResult.code));
+        //         Quagga.stop();
+        //     });
+        //     Quagga.onProcessed(function(result) {
+        //         $("#log-result").html("process: " + JSON.stringify(result));
+        //     });
         // });
 
-        // Quagga.onProcessed(function(result) {
-        //     var drawingCtx = Quagga.canvas.ctx.overlay,
-        //         drawingCanvas = Quagga.canvas.dom.overlay;
+        Quagga.init({
+            inputStream: {
+                name: "Live",
+                type: "LiveStream",
+                target: document.querySelector('#interactive'),
+                constraints: {
+                    width: screen.width,
+                    height: 300,
+                    facingMode: "environment",
+                },
+                area: { // defines rectangle of the detection/localization area
+                    top: "0%", // top offset
+                    right: "0%", // right offset
+                    left: "0%", // left offset
+                    bottom: "0%" // bottom offset
+                },
+            },
+            numOfWorkers: navigator.hardwareConcurrency,
+            locate: true,
+            frequency: 1,
+            debug: {
+                drawBoundingBox: true,
+                showFrequency: true,
+                drawScanline: true,
+                showPattern: true
+            },
+            multiple: false,
+            locator: {
+                halfSample: false,
+                patchSize: "large", // x-small, small, medium, large, x-large
+                debug: {
+                    showCanvas: true,
+                    showPatches: true,
+                    showFoundPatches: true,
+                    showSkeleton: true,
+                    showLabels: true,
+                    showPatchLabels: true,
+                    showRemainingPatchLabels: true,
+                    boxFromPatches: {
+                        showTransformed: true,
+                        showTransformedBox: true,
+                        showBB: true
+                    }
+                }
+            },
+            decoder: {
+                readers: ["code_128_reader"]
 
-        //     if (result) {
-        //         $("#btn-act").html('Processing...');
-        //         if (result.boxes) {
-        //             drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-        //             result.boxes.filter(function(box) {
-        //                 return box !== result.box;
-        //             }).forEach(function(box) {
-        //                 Quagga.ImageDebug.drawPath(box, {
-        //                     x: 0,
-        //                     y: 1
-        //                 }, drawingCtx, {
-        //                     color: "green",
-        //                     lineWidth: 2
-        //                 });
-        //             });
-        //         }
+            }
+        }, function(err) {
+            if (err) {
+                $("#log-result").html("error: " + JSON.stringify(err));
+            }
 
-        //         if (result.box) {
-        //             Quagga.ImageDebug.drawPath(result.box, {
-        //                 x: 0,
-        //                 y: 1
-        //             }, drawingCtx, {
-        //                 color: "#00F",
-        //                 lineWidth: 2
-        //             });
-        //         }
+            Quagga.start();
+        });
 
-        //         if (result.codeResult && result.codeResult.code) {
-        //             Quagga.ImageDebug.drawPath(result.line, {
-        //                 x: 'x',
-        //                 y: 'y'
-        //             }, drawingCtx, {
-        //                 color: 'red',
-        //                 lineWidth: 3
-        //             });
-        //         }
-        //     }
-        // });
+        Quagga.onProcessed(function(result) {
+            var drawingCtx = Quagga.canvas.ctx.overlay,
+                drawingCanvas = Quagga.canvas.dom.overlay;
 
-        // Quagga.onDetected(function(result) {
-        //     var code = result.codeResult.code;
-        //     alert("Barcode Reader value : " + code);
-        //     $("#btn-act").html('Kembali');
-        //     Quagga.stop();
-        // });
+            if (result) {
+                $("#btn-act").html('Processing...');
+                $("#log-result").html("processing: " + JSON.stringify(result));
+                if (result.boxes) {
+                    drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                    result.boxes.filter(function(box) {
+                        return box !== result.box;
+                    }).forEach(function(box) {
+                        Quagga.ImageDebug.drawPath(box, {
+                            x: 0,
+                            y: 1
+                        }, drawingCtx, {
+                            color: "green",
+                            lineWidth: 2
+                        });
+                    });
+                }
+
+                if (result.box) {
+                    Quagga.ImageDebug.drawPath(result.box, {
+                        x: 0,
+                        y: 1
+                    }, drawingCtx, {
+                        color: "#00F",
+                        lineWidth: 2
+                    });
+                }
+
+                if (result.codeResult && result.codeResult.code) {
+                    Quagga.ImageDebug.drawPath(result.line, {
+                        x: 'x',
+                        y: 'y'
+                    }, drawingCtx, {
+                        color: 'red',
+                        lineWidth: 3
+                    });
+                }
+            }
+        });
+
+        Quagga.onDetected(function(result) {
+            var code = result.codeResult.code;
+            $("#log-result").html("detected: " + JSON.stringify(code));
+            $("#btn-act").html('Kembali');
+            Quagga.stop();
+        });
     </script>
 </body>
 
