60882527d6507b8efc525652c9e84844e06bd530
muhammad.yusuf <muhammad.yusuf@immobisp.com>
2024-05-27 18:03:35 +0700

    bug fix

--- app/Http/Controllers/PresenceController.php
+++ app/Http/Controllers/PresenceController.php
@@ -94,7 +94,7 @@ class PresenceController extends Controller
                         'barcode_in' => $barcode,
                         'sign_in' => $sign_in,
                         'updated_by' => auth()->user()->fullname,
-                        'metadata' => $_SERVER['HTTP_USER_AGENT'],
+                        // 'metadata' => $_SERVER['HTTP_USER_AGENT'],
                         'is_late' => $is_late
                     ]);
                     return json_encode(['status' => true, 'message' => $inserted]);
