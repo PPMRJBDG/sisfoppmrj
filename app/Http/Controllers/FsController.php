3e36ae6d9c7c6b3ec37553a2c9644fe48123fb2f
Muhammad Yusuf <muhmdysf@gmail.com>
2025-03-07 20:24:16 +0700

    update

--- app/Http/Controllers/FsController.php
+++ app/Http/Controllers/FsController.php
@@ -16,34 +16,31 @@ class FsController extends Controller
     public function fs01()
     {
         $original_data  = file_get_contents('php://input');
-        $decoded_data   = json_decode($original_data, true);
-        $encoded_data   = json_encode($decoded_data);
+        $decoded_data   = json_decode($original_data);
         // WaSchedules::save('Testing', 'Masuk FS01 - Fingerprint', 'wa_ketertiban_group_id');
 
-        if (isset($decoded_data['type']) AND isset($decoded_data['cloud_id'])){
-            $type       = $decoded_data['type'];
-            $cloud_id   = $decoded_data['cloud_id'];
-            $created_at = date('Y-m-d H:i:s');
+        $type       = $decoded_data['type'];
+        $cloud_id   = $decoded_data['cloud_id'];
+        $created_at = date('Y-m-d H:i:s');
 
-            FsLogs::create([
-                'cloud_id' => $cloud_id,
-                'type' => $type,
-                'created_at' => $created_at,
-                'original_data' => $encoded_data
-            ]);
+        FsLogs::create([
+            'cloud_id' => $cloud_id,
+            'type' => $type,
+            'created_at' => $created_at,
+            'original_data' => json_encode($decoded_data)
+        ]);
 
-            // if($type=='attlog'){
-            //     echo "OK";
-            // }elseif($type=='set_userinfo'){
-            //     echo "OK";
-            // }elseif($type=='get_userinfo'){
-            //     $pin_santri_id   = $decoded_data['data']['pin'];
-            //     $santri = Santri::find($pin_santri_id);
-            //     $santri->template_fs = $decoded_data['data']['template'];
-            //     $santri->save();
-            //     echo "OK";
-            // }
-        }
+        // if($type=='attlog'){
+        //     echo "OK";
+        // }elseif($type=='set_userinfo'){
+        //     echo "OK";
+        // }elseif($type=='get_userinfo'){
+        //     $pin_santri_id   = $decoded_data['data']['pin'];
+        //     $santri = Santri::find($pin_santri_id);
+        //     $santri->template_fs = $decoded_data['data']['template'];
+        //     $santri->save();
+        //     echo "OK";
+        // }
     }
 }
 ?>
