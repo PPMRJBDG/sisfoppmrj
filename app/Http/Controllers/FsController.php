7ede5bb1f52c5887781764200e74e0308f249182
Muhammad Yusuf <muhmdysf@gmail.com>
2025-03-07 15:00:44 +0700

    update

--- app/Http/Controllers/FsController.php
+++ app/Http/Controllers/FsController.php
@@ -18,12 +18,12 @@ class FsController extends Controller
         $original_data  = file_get_contents('php://input');
         $decoded_data   = json_decode($original_data, true);
         $encoded_data   = json_encode($decoded_data);
+        WaSchedules::save('Testing', 'Masuk FS01 - Fingerprint', 'wa_ketertiban_group_id');
 
         if (isset($decoded_data['type']) AND isset($decoded_data['cloud_id'])){
             $type       = $decoded_data['type'];
             $cloud_id   = $decoded_data['cloud_id'];
             $created_at = date('Y-m-d H:i:s');
-            WaSchedules::save('Testing', 'Masuk FS01 - Fingerprint', 'wa_ketertiban_group_id');
 
             if($type=='set_userinfo'){
                 echo "OK - SET USER INFO";
